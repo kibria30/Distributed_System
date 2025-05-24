57e720a6e815b5a7d47bfa80060bd8711513af2e
kibria30 <kibria8007@gmail.com>
2025-05-24 19:57:31 +0600

    Monolith to mocroservice done

---
+++ smart_library_microservice/loan_service/src/database/__init__.py
@@ -0,0 +1,7 @@
+from .session import engine
+from .base import Base
+from src.entities.loan import Loan
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
